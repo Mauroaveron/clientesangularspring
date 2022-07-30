9ec5f3da2d953bf7b60cde056a30cc7b74e60e78
PEI-user30 <mauroaveron@gmail.com>
2022-07-30 17:14:39 -0300

    first commit

---
+++ angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente } from './cliente';
+
+  export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Andrés', apellido: 'Guzmán', email: 'profesor@bolsadeideas.com', createAt: '2017-12-11'},
+    {id: 2, nombre: 'José', apellido: 'Guzmán', email: 'profesor@bolsadeideas.com', createAt: '2017-12-12'},
+    {id: 3, nombre: 'Carlos', apellido: 'Guzmán', email: 'profesor@bolsadeideas.com', createAt: '2017-12-15'},
+    {id: 4, nombre: 'Mauro', apellido: 'Guzmán', email: 'profesor@bolsadeideas.com', createAt: '2017-12-21'},
+    {id: 5, nombre: 'Mariano', apellido: 'Guzmán', email: 'profesor@bolsadeideas.com', createAt: '2017-12-31'},
+    {id: 6, nombre: 'Pablo', apellido: 'Guzmán', email: 'profesor@bolsadeideas.com', createAt: '2017-12-05'}
+  ];
